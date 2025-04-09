2dc8a4346a5e4ca2772e7ed8cfa55f82bdf660b1
Niklas Vorberg <services@vorberg-webmail.de>
2025-04-09 12:04:03 +0000

    TASK: added about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import NavigationBar from '../components/NavigationBar';
+
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <NavigationBar />
+    </div>
+  );
+}
